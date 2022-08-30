0789ae890805013a67da7a3687608a7c5ed8aa5f
sadathimel <sadat.himel@gmail.com>
2022-08-30 17:41:16 +0600

    Features Section updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSuperLight: 'hsl(12, 88%, 95%)',
+        brightRedSupLight: 'hsl(12, 88%, 79%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(228, 12%, 13%)',
